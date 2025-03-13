25a57c5c2edabd9768ffc203579f94323d8e75c8
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-13 15:22:42 +0100

    feat(transactions)*: implement transactions POST api route

---
+++ app/api/transactions/addTransaction/route.ts
@@ -0,0 +1,39 @@
+import { getCollection } from "@/app/utils/mongodb";
+import { NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+import { ObjectId } from 'mongodb';
+
+export async function POST(request: Request) {
+    try {
+        const { name, type, amount, userId } = await request.json();
+
+        if (!name || !type || !amount || !userId) {
+            return NextResponse.json({ error: "Name, type, amount, and userId are required" }, { status: 400 });
+        }
+
+        if (type !== 'income' && type !== 'expense') {
+            return NextResponse.json({ error: "Invalid type" }, { status: 400 });
+        }
+
+        const transactionsCollection = getCollection('transactions');
+
+        const newTransaction = {
+            name,
+            type,
+            amount: parseFloat(amount), // Ensure amount is a number
+            userId: new ObjectId(userId), //store the user id as an object Id
+            createdAt: new Date(),
+        };
+
+        const result = await transactionsCollection.insertOne(newTransaction);
+
+        if (!result.acknowledged) {
+            return NextResponse.json({ error: "Failed to add transaction" }, { status: 500 });
+        }
+
+        return NextResponse.json({ message: "Transaction added successfully", transactionId: result.insertedId }, { status: 201 });
+    } catch (error) {
+        console.error('Add transaction error: ', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    }
+}
